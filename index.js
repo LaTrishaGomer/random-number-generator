914ab617e3795db8e71048b147b9380ba09cfe96
LaTrisha <L_gomer@yahoo.com>
2024-12-26 11:53:21 -0800

    updated files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const label1 = document.getElementById("label1");
 const label2 = document.getElementById("label2");
 const label3 = document.getElementById("label3");
 const min = 1;
-const max = 6;
+const max = 9;
 let randomNum1;
 let randomNum2;
 let randomNum3;
